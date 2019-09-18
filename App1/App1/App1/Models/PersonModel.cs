55867dd076644469bc09197ab565e78b1e89d1fd
Burak KIZILTAŞ <burak_kiziltas1907@hotmail.com>
2019-09-18 23:58:06 +0300

    İlk Yükleme

---
+++ App1/App1/App1/Models/PersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace App1.Models
+{
+    public class PersonModel:INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        //30 dakikada kaldım
+        void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ App1/App1/App1/Vİews/PersonPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using App1.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Vİews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonPage : ContentPage
+    {
+        private readonly IList<PersonModel> model = new ObservableCollection<PersonModel>();
+        public PersonPage()
+        {
+            InitializeComponent();
+            BindData();
+        }
+        private void BindData()
+        {
+            model.Add(new PersonModel
+            {
+                Id=1,
+                Name="Burak",
+                Surname="Kızıltaş"
+            });
+            model.Add(new PersonModel
+            {
+                Id = 2,
+                Name = "Burak",
+                Surname = "Kızıltaş"
+            });
+            lstPerson.BindingContext = model;
+        }
+
+        private void onDelete(object sender, EventArgs e)
+        {
+            MenuItem item = (MenuItem)sender;
+            PersonModel selectedModel = (PersonModel)item.CommandParameter;
+            if (selectedModel!=null)
+            {
+                model.Remove(selectedModel);
+            }
+        }
+
+        private void onUpdate(object sender, EventArgs e)
+        {
+            MenuItem item = (MenuItem)sender;
+            PersonModel selectedModel = (PersonModel)item.CommandParameter;
+            if (selectedModel != null)
+            {
+                selectedModel.Name = "dasdasd";
+            }
+        }
+    }
+}
